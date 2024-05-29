024cb078633b7984c145918e62a73105ab47bf3f
Ayush Singh <ayushsingh2442@gmail.com>
2024-05-29 14:18:23 +0530

    fixed watchlist issues

--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -3,15 +3,17 @@ import Header from '../components/Common/Header/Header'
 import DashboardPage from '../components/Dasboard/DashboardPage'
 import TabsComponent from '../components/Dasboard/Tabs/TabsComponent'
 import { allCoinData } from '../functions/getAllCoinData';
+import Button from '../components/Button/Button';
+import { Link } from 'react-router-dom';
 
 function Watchlist() {
     const [coin, setCoin] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const watchlistCoin = JSON.parse(localStorage.getItem('watchlist'));
-    
+
     useEffect(() =>{
         getAllCoin();
-    },[watchlistCoin])
+    },[watchlistCoin,coin])
 
     const getAllCoin = async () => {
         const data = await allCoinData();
@@ -24,7 +26,13 @@ function Watchlist() {
   return (
     <div>
         <Header/>
-        <TabsComponent  coins={coin}/>
+        
+        {coin.length != 0 ?  (<TabsComponent  coins={coin}/>) : (<div className='flex flex-col justify-center items-center h-screen '>
+          
+            <div><h1 className='text-7xl text-blue font-semibold py-4'>No item </h1>
+            </div>
+            <Link to='/dashboard'><Button text={"dashboard"} /></Link>
+          </div>)}
 
     </div>
   )
